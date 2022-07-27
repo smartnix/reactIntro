3eb8c81dc8dd87534070c7d8b9585112dec5b1b0
smartnix <smartnix@hotmail.com>
2022-07-26 20:52:06 -0500

    En index.js se creo un ejemplo de funciones de alto nivel con componenetes y atributos

--- src/index.js
+++ src/index.js
@@ -3,10 +3,40 @@ import ReactDOM from 'react-dom'; //Esto se cambia
 import './index.css';
 
 import React from 'react';
-import App from './App/App';
+// import App from './App/App';
+
+function App(props) {
+  return (
+    <h1>Hey, {props.saludo} {props.tardes}, {props.nombre} </h1>
+  )
+}
+
+function withSaludo(saludo) 
+{
+  return function WrappedComponentWithSaludo(WrappedComponent)
+  {
+    return function otherFunc(tardes) 
+    {
+      return function (props) 
+      {
+        return (
+          <React.Fragment>
+            <WrappedComponent {...props} saludo={saludo} tardes={tardes}/>
+            <p>Estamos acompañando al WrappedComponent</p>
+          </React.Fragment>
+        )
+      }
+    }
+  }
+}
+
+const AppwithSaludo = withSaludo('buenas')(App)('tardes');
+
 
 ReactDOM.render( //esto tambien se cambia
-    <App />,
+    // <App saludo="Hola" nombre="smart"/>,
+    <AppwithSaludo nombre="Mariana"/>,
+
   document.getElementById('root')
 );
 
